15c1ffba160e136ba211ba83e748accda7c58711
iagatha <iagathasouzaa@gmail.com>
2023-06-15 14:33:35 -0300

    type string

---
+++ modulo-2/type/typeString.ts
@@ -0,0 +1,19 @@
+// Single Quotes
+let nomeCompleto : string = 'Iagatha Souza';
+console.log(nomeCompleto);
+
+let animal : string = 'Panda';
+console.log(animal);
+
+// Double Quotes
+
+let funcaoEmpresa : string = "Developer";
+console.log(funcaoEmpresa);
+
+// Back Ticks
+
+let nomeEmpresa : string = 'Bank';
+let dadosFuncionario : string = `Seja bem vindo ${nomeCompleto}! Você será ${funcaoEmpresa}
+ na empresa ${nomeEmpresa}`;
+
+console.log(dadosFuncionario);
