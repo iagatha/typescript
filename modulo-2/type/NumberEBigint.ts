9c2f0bc993082bb6d238986fd9706d9227fc8e13
iagatha <iagathasouzaa@gmail.com>
2023-06-15 14:15:01 -0300

    type bigint

--- modulo-2/type/NumberEBigint.ts
+++ modulo-2/type/NumberEBigint.ts
@@ -1,3 +1,5 @@
+// Exemplos number
+
 let num1 : number = 23.0; 
 let num2 : number = 0x78CF; 
 let num3 : number = 0b1010100; 
@@ -8,3 +10,12 @@ console.log('Numero binário', num3);
 
 // Exemplos Bigint
 
+let big1 : bigint = 900648639029825383n;
+let big2 : bigint = 0b100000000000000000000000000000000000000000000011n;
+let big3 : bigint = 0x2000000000000003n;
+let big4 : bigint = 0o40000000000000000003n;
+
+console.log('Bigint' , big1);
+console.log('Bigint Binário' ,big2);
+console.log('Bigint Hexadecimal',  big3);
+console.log('Bigint Octal', big4);
