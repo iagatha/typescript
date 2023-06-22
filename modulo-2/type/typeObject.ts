89aa1df21bea049e99ea7158ad3957f17dafe08c
iagatha <iagathasouzaa@gmail.com>
2023-06-22 15:03:30 -0300

    object

--- modulo-2/type/typeObject.ts
+++ modulo-2/type/typeObject.ts
@@ -84,3 +84,106 @@ function onboarding04(pessoa : Pessoa04){
 }
 
 console.log(onboarding04({nome : 'Iagatha', funcao: 'Desenvolvedora', linguagem : 'Java'}));
+
+// Propriedade 'readonly' (se deseja proibir que não modifique um determinado objeto use o readonly)
+
+
+interface Pessoa05{
+    nome : string;
+    funcao : string;
+    linguagem : string;
+    readonly email : string; // 
+}
+
+function onboarding05(pessoa : Pessoa04){
+    return (
+        'Bem vinda ' + 
+        pessoa.nome +
+        '! ' +
+        'Sua funcao aqui será ' +
+        pessoa.funcao +
+        ' . ' +
+        'Você trabalhará com a linguagem : ' +
+        pessoa.linguagem + ' . ' +
+        'Seu email será: ' + pessoa.email
+    );
+}
+
+console.log(onboarding05(
+    { nome : 'Iagatha',
+ funcao: 'Desenvolvedora',
+  linguagem : 'Java',
+  email : 'hzdkv@example.com'
+}));
+
+// Tipos de extensões (herança)
+
+interface Mae{
+    nome : string;
+}
+
+interface Pai {
+    sobrenome : string;
+}
+
+interface Filho extends Mae, Pai{
+   idade : number;
+}
+
+const filho : Filho = {
+    nome: 'josé',
+    sobrenome : 'carlos',
+    idade : 23
+};
+
+console.log(filho);
+
+// Tipo de interseções
+
+interface Cachorro {
+    tipo : string;
+}
+
+interface Gato {
+    tipo : string;
+}
+
+type Animal = Cachorro & Gato;filho
+
+// Generic Objects
+
+type Admin = {
+    nome : string;
+    email : string;
+    admin : true;
+}
+
+type Usuario = {
+    nome : string;
+    email : string;
+}
+
+const usuario : Usuario = {
+    nome : 'Iagatha',
+    email : 'hzdkv@example.com'
+};
+
+const admin : Admin = {
+    nome : 'Iagatha',
+    email : 'hzdkv@example.com',
+    admin : true
+}
+
+function acessarSistema<T>(usuario : T) : T{ // Generic T
+    return usuario;
+}
+
+console.log(acessarSistema<Usuario>(usuario));
+console.log(acessarSistema<Admin>(admin));
+
+
+/*function acessarSistema(usuario : Usuario) : Usuario {
+    return usuario;
+} */
+
+//console.log(acessarSistema(usuario));
