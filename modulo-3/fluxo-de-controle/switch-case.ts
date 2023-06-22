28a68f0a16eac018a7917a6a150404095efc2e44
iagatha <iagathasouzaa@gmail.com>
2023-06-22 17:02:10 -0300

    fluxos de controle: switch case

---
+++ modulo-3/fluxo-de-controle/switch-case.ts
@@ -0,0 +1,16 @@
+let flor = 'tulipa';
+
+switch (flor) {
+    case 'rosa':
+        console.log('Rosa são vermelhas');
+    break;
+    case 'violeta':
+        console.log('Violeta são azuis');
+    break;
+    case 'tulipa':
+        console.log('Tulipa são brancas');
+    break;
+    
+    default:
+        console.log('Opção inválida, selecione outra flor');
+}
